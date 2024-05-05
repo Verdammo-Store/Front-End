adf44a3a5436e9166f758213179d8c4bfc91aad9
Manuel <manuel.neto@ufms.br>
2024-05-05 12:25:27 -0300

    layout da tela de cadastro de usuario criada

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "../pages/login";
+import CadastroUser from "../pages/cadastroUser";
 
 const Rotas = () => {
   return (
     <Router>
       <Routes>
         <Route path="/*" element={<Login />} />
-        
+        <Route path="/cadastroUser" element={<CadastroUser />} />
       </Routes>
     </Router>
   );
